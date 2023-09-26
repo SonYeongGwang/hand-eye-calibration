ba1ed5d6c896c1831aee1fc9948209447792e17a
sim <sim30217@gmail.com>
2023-09-26 21:20:43 +0900

    Refactoring_getcal

--- scripts/get_calibration_result.py
+++ scripts/get_calibration_result.py
@@ -1,21 +1,29 @@
-import sys
-import cv2
-import yaml
-import numpy as np
-import pprint
 import os
+import pprint
 from ruamel.yaml import YAML
-yaml = YAML()
-yaml.default_flow_style = None
-log_file1 = os.path.expanduser('~/catkin_ws/src/hand-eye-calibration/calibration/right.yml')
-log_file2 = os.path.expanduser('~/catkin_ws/src/hand-eye-calibration/calibration/left.yml')
+import numpy as np
+import cv2
 
-def load_poses(file_path):
+# Define constants for file paths
+CALIBRATION_DIR = os.path.expanduser('~/catkin_ws/src/hand-eye-calibration/calibration')
+TASK_CONFIG_PATH = os.path.expanduser('~/catkin_ws/src/suction_net_ros/config/task_config.yml')
+
+def load_yaml(file_path: str) -> dict:
+    """
+    Load YAML data from a file.
+    """
+    yaml = YAML()
+    yaml.default_flow_style = None
     with open(file_path) as f:
-        pose_stack = yaml.load(f)
-        
-    num_of_poses = len(pose_stack['cam2marker'].keys())
+        data = yaml.load(f)
+    return data
 
+def load_poses(file_path: str):
+    """
+    Load poses from a YAML file.
+    """
+    pose_stack = load_yaml(file_path)
+    num_of_poses = len(pose_stack['cam2marker'].keys())
     rot_a = np.empty((num_of_poses, 3, 3))
     rot_b = np.empty((num_of_poses, 3, 3))
     trans_a = np.empty((num_of_poses, 3, 1))
@@ -29,42 +37,46 @@ def load_poses(file_path):
 
     return rot_a, trans_a, rot_b, trans_b
 
-# Load poses from the two log files
-rot_a1, trans_a1, rot_b1, trans_b1 = load_poses(log_file1)
-rot_a2, trans_a2, rot_b2, trans_b2 = load_poses(log_file2)
+def save_yaml(file_path: str, data: dict):
+    """
+    Save data to a YAML file.
+    """
+    yaml = YAML()
+    yaml.default_flow_style = None
+    with open(file_path, 'w') as f:
+        yaml.dump(data, f)
 
-# Perform hand-eye calibration for the two cameras
-result1 = cv2.calibrateRobotWorldHandEye(rot_a1, trans_a1, rot_b1, trans_b1)
-result2 = cv2.calibrateRobotWorldHandEye(rot_a2, trans_a2, rot_b2, trans_b2)
+def calibrate_camera(log_file: str):
+    """
+    Perform hand-eye calibration for a camera.
+    """
+    rot_a, trans_a, rot_b, trans_b = load_poses(log_file)
+    return cv2.calibrateRobotWorldHandEye(rot_a, trans_a, rot_b, trans_b)
 
-task_path = os.path.expanduser('~/catkin_ws/src/suction_net_ros/config/task_config.yml')
+def main():
+    """
+    Main function to perform calibration and update configuration.
+    """
+    log_file_right = os.path.join(CALIBRATION_DIR, 'right.yml')
+    log_file_left = os.path.join(CALIBRATION_DIR, 'left.yml')
+    
+    result_right = calibrate_camera(log_file_right)
+    result2 = calibrate_camera(log_file_left)
+    
+    config_data = load_yaml(TASK_CONFIG_PATH)
+    
+    homogeneous_matrix_1 = np.vstack((np.hstack((result_right[2], result_right[3])), np.array([0, 0, 0, 1])))
+    homogeneous_matrix_2 = np.vstack((np.hstack((result2[2], result2[3])), np.array([0, 0, 0, 1])))
+    
+    config_data['TF']['base2cam_demo']['base2cam_right'] = np.round(homogeneous_matrix_1, 8).tolist()
+    config_data['TF']['base2cam_demo']['base2cam_left'] = np.round(homogeneous_matrix_2, 8).tolist()
+    
+    save_yaml(TASK_CONFIG_PATH, config_data)
+    
+    pprint.pprint("Calibration Result for Camera 1:")
+    pprint.pprint(result_right)
+    pprint.pprint("Calibration Result for Camera 2:")
+    pprint.pprint(result2)
 
-# Load the task configuration data
-with open(task_path, 'r') as f:
-    config_data = yaml.load(f)
-
-# Convert the results to homogeneous matrices
-homogeneous_matrix_1 = np.hstack((result1[2], result1[3]))
-homogeneous_matrix_1 = np.vstack((homogeneous_matrix_1, np.array([0, 0, 0, 1])))
-homogeneous_matrix_2 = np.hstack((result2[2], result2[3]))
-homogeneous_matrix_2 = np.vstack((homogeneous_matrix_2, np.array([0, 0, 0, 1])))
-
-# Round the elements of the homogeneous matrices to 8 decimal places
-homogeneous_matrix_1 = np.round(homogeneous_matrix_1, 8)
-homogeneous_matrix_2 = np.round(homogeneous_matrix_2, 8)
-
-print(np.shape(homogeneous_matrix_1))
-print(homogeneous_matrix_1)
-
-# Flatten the matrices and update the configuration data
-config_data['TF']['base2cam_demo']['base2cam_right'] = homogeneous_matrix_1.tolist()
-config_data['TF']['base2cam_demo']['base2cam_left'] = homogeneous_matrix_2.tolist()
-
-# Write the updated config data back to the file
-with open(task_path, 'w') as f:
-    yaml.dump(config_data, f)
-
-pprint.pprint("Calibration Result for Camera 1:")
-pprint.pprint(result1)
-pprint.pprint("Calibration Result for Camera 2:")
-pprint.pprint(result2)
+if __name__ == "__main__":
+    main()
